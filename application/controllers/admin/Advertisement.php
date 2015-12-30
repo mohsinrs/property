5cd23863552b3222f745e6fd125f80883b2f8892
waqar <iwaqarsiddiqui@gmail.com>
2015-12-30 17:25:19 +0500

    file upload script

--- application/controllers/admin/Advertisement.php
+++ application/controllers/admin/Advertisement.php
@@ -19,13 +19,17 @@ class advertisement extends Base_Controller {
      * map to /index.php/welcome/<method_name>
      * @see http://codeigniter.com/user_guide/general/urls.html
      */
+    function __construct() {
+        parent::__construct();
+        $this->load->helper(array('form', 'url'));
+    }
+
     public function index() {
 
         $this->load->model('advertisement_model');
         $data = array();
         $data['result'] = $this->advertisement_model->fetchAll();
         $data['title'] = "All Advertisement";
-
         $this->render('admin/advertisement/index', $data);
     }
 
@@ -62,7 +66,6 @@ class advertisement extends Base_Controller {
                 $data['result'] = $result[0];
             }
         }
-
         $data['title'] = ($id == NULL) ? "New Advertisement" : "Edit Advertisement";
         $this->render('admin/advertisement/view');
     }
@@ -86,22 +89,23 @@ class advertisement extends Base_Controller {
         }
     }
 
-    public function do_upload($id) {
-        if (empty($id)) {
-            setNotification('notice', 'Record not found');
-            redirect(base_url('admin/advertisement'));
-        }
+    function do_upload() {
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['max_size'] = '100';
+        $config['max_width'] = '1024';
+        $config['max_height'] = '768';
 
-        try {
-            $this->load->model('advertisement_model');
-            $result = $this->advertisement_model->do_upload($id);
-            if ($result == true) {
-                setNotification('success', 'Record deleted successfully');
-                redirect(base_url('admin/advertisement'));
-            }
-        } catch (Exception $e) {
-            setNotification('error', 'Error in deleting record');
-            redirect(base_url('admin/advertisement'));
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload()) {
+            $error = array('error' => $this->upload->display_errors());
+
+            $this->load->view('view', $error);
+        } else {
+            $data = array('upload_data' => $this->upload->data());
+
+            $this->load->view('index', $data);
         }
     }
 
--- application/models/advertisement_model.php
+++ application/models/advertisement_model.php
@@ -61,14 +61,4 @@ class Advertisement_model extends CI_Model {
         return ($this->db->affected_rows() != 1) ? false : true;
     }
 
-    function do_upload($id) {
-        $config['upload_path'] = './uploads/';
-        $config['allowed_types'] = 'gif|jpg|png';
-        $config['max_size'] = '100';
-        $config['max_width'] = '1024';
-        $config['max_height'] = '768';
-
-        $this->load->library('upload', $config);
-    }
-
 }
