85e46fb0df126973112e96700132d86b5ef054b2
Mohsin Rafiq <mohsin_rafiq@live.com>
2016-01-06 09:34:07 +0500

    - Front-End temporary pages added

---
+++ application/views/search.php
@@ -0,0 +1,158 @@
+<div class="main">
+    <div class="container">
+        <!-- BEGIN SIDEBAR & CONTENT -->
+        <div class="row margin-bottom-40">          
+            <!-- BEGIN CONTENT -->
+            <div class="col-md-9 col-sm-7">
+                <!-- BEGIN PRODUCT LIST -->
+                <div class="row product-list">
+                    <!-- PRODUCT ITEM START -->
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="product-item row">
+                            <div class="pi-img-wrapper col-md-4">
+                                <img src="<?php echo base_url('/public/frontend/pages/img/products/1.jpg') ?>" class="img-responsive" alt="Berry Lace Dress">
+                            </div>
+                            <div class="col-md-6">
+                                <h3><a href="#"><b>DHA Phase 3 - Block Y, DHA Phase 3</b></a></h3>
+                                <p>5 bedrooms - 1 Kanal</p>
+                                <p>Beautiful bungalow for sale. Location: DHA Phase 3 - Block Y, Lahore. It has 5 bedrooms. Facing ... </p>
+                                <p> &nbsp;</p>
+                                <p><i class="fa fa-heart"></i> Save Listing</p>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="pi-price">Rs 63,50,000</div>
+                                <br><br>
+                                <img class="img-responsive" src="<?php echo base_url('/public/frontend/pages/img/people/img8-large.jpg') ?>">
+                                <br>
+                                <a href="<?php echo base_url('agent/index/1'); ?>" class="btn btn-default add2cart">View Detail</a></div>
+                        </div>
+                    </div>
+                    <!-- PRODUCT ITEM END -->
+                    <!-- PRODUCT ITEM START -->
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="product-item row">
+                            <div class="pi-img-wrapper col-md-4">
+                                <img src="<?php echo base_url('/public/frontend/pages/img/products/2.jpg') ?>" class="img-responsive" alt="Berry Lace Dress">
+                            </div>
+                            <div class="col-md-6">
+                                <h3><a href="#"><b>DHA Phase 3 - Block Y, DHA Phase 3</b></a></h3>
+                                <p>5 bedrooms - 1 Kanal</p>
+                                <p>Beautiful bungalow for sale. Location: DHA Phase 3 - Block Y, Lahore. It has 5 bedrooms. Facing ... </p>
+                                <p> &nbsp;</p>
+                                <p><i class="fa fa-heart"></i> Save Listing</p>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="pi-price">Rs 63,50,000</div>
+                                <br><br>
+                                <img class="img-responsive" src="<?php echo base_url('/public/frontend/pages/img/people/img8-large.jpg') ?>">
+                                <br>
+                                <a href="<?php echo base_url('agent/index/1'); ?>" class="btn btn-default add2cart">View Detail</a></div>
+                        </div>
+                    </div>
+                    <!-- PRODUCT ITEM END -->
+                    <!-- PRODUCT ITEM START -->
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="product-item row">
+                            <div class="pi-img-wrapper col-md-4">
+                                <img src="<?php echo base_url('/public/frontend/pages/img/products/3.jpg') ?>" class="img-responsive" alt="Berry Lace Dress">
+                            </div>
+                            <div class="col-md-6">
+                                <h3><a href="#"><b>DHA Phase 3 - Block Y, DHA Phase 3</b></a></h3>
+                                <p>5 bedrooms - 1 Kanal</p>
+                                <p>Beautiful bungalow for sale. Location: DHA Phase 3 - Block Y, Lahore. It has 5 bedrooms. Facing ... </p>
+                                <p> &nbsp;</p>
+                                <p><i class="fa fa-heart"></i> Save Listing</p>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="pi-price">Rs 63,50,000</div>
+                                <br><br>
+                                <img class="img-responsive" src="<?php echo base_url('/public/frontend/pages/img/people/img8-large.jpg') ?>">
+                                <br>
+                                <a href="<?php echo base_url('agent/index/1'); ?>" class="btn btn-default add2cart">View Detail</a></div>
+                        </div>
+                    </div>
+                    <!-- PRODUCT ITEM END -->
+                </div>
+                <!-- END PRODUCT LIST -->
+                <!-- BEGIN PAGINATOR -->
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 items-info">Items 1 to 9 of 10 total</div>
+                    <div class="col-md-8 col-sm-8">
+                        <ul class="pagination pull-right">
+                            <li><a href="javascript:;">&laquo;</a></li>
+                            <li><a href="javascript:;">1</a></li>
+                            <li><span>2</span></li>
+                            <li><a href="javascript:;">3</a></li>
+                            <li><a href="javascript:;">4</a></li>
+                            <li><a href="javascript:;">5</a></li>
+                            <li><a href="javascript:;">&raquo;</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- END PAGINATOR -->
+            </div>
+            <!-- END CONTENT -->
+            <!-- BEGIN SIDEBAR -->
+            <div class="sidebar col-md-3 col-sm-5">
+                <h2>Search Property</h2>
+                <!-- BEGIN FORM-->
+                <form action="#" role="form">
+
+                    <div class="form-group">
+                        <label>Property Purpose</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Property Price</label>
+                        <select class="form-control">
+                            <option>Any</option>
+                            <option>Rs. 1,00,000-5,00,000</option>
+                            <option>Rs. 5,00,000-10,00,000</option>
+                        </select>                    
+                    </div>
+
+                    <div class="form-group">
+                        <label>Property Type</label>
+                        <select class="form-control">
+                            <option>Any</option>
+                            <option>Residential</option>
+                            <option>Commercial</option>
+                        </select>                    
+                    </div>
+                    <div class="form-group">
+                        <label>Property Location</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label> &nbsp;</label>
+                        <button type="submit" class="btn btn-primary"><i class="icon-search"></i>Search</button>
+                    </div>
+
+                </form>
+                <!-- END FORM-->
+
+                <div class="sidebar-products clearfix">
+                    <h2>Featured Property</h2>
+                    <div class="item">
+                        <a href="shop-item.html"><img src="<?php echo base_url('/public/frontend/pages/img/products/5.jpg') ?>" alt="Some Shoes in Animal with Cut Out"></a>
+                        <h3><a href="shop-item.html">Some Shoes in Animal with Cut Out</a></h3>
+                        <div class="price">$31.00</div>
+                    </div>
+                    <div class="item">
+                        <a href="shop-item.html"><img src="<?php echo base_url('/public/frontend/pages/img/products/4.jpg') ?>" alt="Some Shoes in Animal with Cut Out"></a>
+                        <h3><a href="shop-item.html">Some Shoes in Animal with Cut Out</a></h3>
+                        <div class="price">$23.00</div>
+                    </div>
+                    <div class="item">
+                        <a href="shop-item.html"><img src="<?php echo base_url('/public/frontend/pages/img/products/6.jpg') ?>" alt="Some Shoes in Animal with Cut Out"></a>
+                        <h3><a href="shop-item.html">House in Islamabad</a></h3>
+                        <div class="price">Sector E-7</div>
+                    </div>
+                </div>
+            </div>
+            <!-- END SIDEBAR -->
+        </div>
+        <!-- END SIDEBAR & CONTENT -->
+    </div>
+</div>
